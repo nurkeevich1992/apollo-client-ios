432b31493332126a13a2069deb820ffe7f099043
K-Bermet <bkalbotoeva@gmail.com>
2020-01-17 14:24:20 -0600

    Initial Commit

---
+++ AuthDemoTry/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AuthDemoTry
+//
+//  Created by tilekbek kadyrov on 1/17/20.
+//  Copyright © 2020 tilekbek kadyrov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
